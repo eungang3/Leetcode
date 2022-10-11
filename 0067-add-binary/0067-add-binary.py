655cbb5082f9d98c5d2460e9b91be44c18579798
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-11 17:49:13 +0900

    Time: 58 ms (53.33%), Space: 13.9 MB (25.61%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,24 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        int_a = 0
-        int_b = 0
-
-        for i in range(len(a)):
-            power = len(a) - i - 1
-            int_a += (2 ** power) * int(a[i])
-
-        for j in range(len(b)):
-            power = len(b) - j - 1
-            int_b += (2 ** power) * int(b[j])
-
-        total = int_a + int_b
-        result = ''
-
-        while(total > 0):
-            result += str(total % 2)
-            total = total // 2
-        
-        if result == "":
-            return "0"
-            
-        return result[::-1]
+        return str(bin(int(a, 2) + int(b, 2)))[2::]
