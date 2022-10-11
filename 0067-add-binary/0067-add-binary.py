08c8a7df9d86bc9bd45f71a54200d118336db7bf
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-11 18:02:53 +0900

    Time: 75 ms (17.18%), Space: 13.9 MB (25.61%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,26 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return str(bin(int(a, 2) + int(b, 2)))[2::]
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+
+            if b:
+                carry += int(b.pop())
+
+            # carry가 0이면 0을 result에 추가
+            # carry가 1이면 1을 result에 추가
+            # carry가 2면 0을 result에 추가
+            result += str(carry % 2)
+
+            # carry가 0이면 carry는 0이 됨
+            # carry가 1이면 carry는 0이 됨
+            # carry가 2면 carry는 1이 됨
+            carry //= 2
+
+        return result[::-1]      
