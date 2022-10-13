13f91301fe5a31c6e677030186a820ab210b87a2
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-14 00:11:16 +0900

    Time: 57 ms, Memory: 13.9 MB - LeetHub

---
+++ implement-strstr/implement-strstr.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        needle_length, haystack_length = len(needle), len(haystack)
+        if needle_length > haystack_length:
+            return -1
+        if needle_length == 0:
+            return 0
+        
+        for i in range(haystack_length - needle_length + 1):
+            if haystack[i:i+needle_length] == needle:
+                return i
+        return -1
