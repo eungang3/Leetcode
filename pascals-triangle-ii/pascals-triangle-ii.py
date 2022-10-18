41fea01ce138a5d3d8f3f528a679c686a5fcaf14
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-18 10:41:30 +0900

    Time: 75 ms, Memory: 13.9 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        i = 0
+        result = []
+        
+        while i <= rowIndex:
+            if i == 0:
+                result.append([1])
+                i += 1
+                
+            else:
+                current = [1]
+                prev = result[i - 1]
+                for j in range(len(prev)):
+                    if j + 1 < len(prev):
+                        current.append(prev[j] + prev[j + 1])
+                current.append(1)
+                result.append(current)
+                i += 1
+        return result[rowIndex]
