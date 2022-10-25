7771aac3b13880e9780183fda10c1f216146bf86
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-25 15:35:53 +0900

    Time: 169 ms (16.80%), Space: 14.6 MB (57.34%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,59 +1,50 @@
 class MyCircularQueue:
-
     def __init__(self, k: int):
         self.max_length = k
         self.current_length = 0
-        self.queue = [None] * k
-        self.head = -1
-        self.tail = -1
+        self.queue = [None] * k 
+        self.front_index = -1 
+        self.rear_index = -1 
 
     def enQueue(self, value: int) -> bool:
+         # 1) 큐에 자리 있는지 확인
         if self.isFull():
             return False
         
-        tail = (self.tail + 1) % self.max_length
-        self.queue[tail] = value
-        self.tail = tail
+        # 2) 
+        rear_index = (self.rear_index + 1) % self.max_length
+        self.queue[rear_index] = value
+        self.rear_index = rear_index
         self.current_length += 1
         if self.current_length == 1:
-            self.head = 0
+            self.front_index = 0
+
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
         
-        self.head = (self.head + 1) % self.max_length
+        self.front_index = (self.front_index + 1) % self.max_length
         self.current_length -= 1
         if self.isEmpty():
-            self.head = -1
-            self.tail = -1
+            self.front_index = -1
+            self.rear_index = -1
         
         return True
-        
 
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[self.head]
+        return self.queue[self.front_index]
 
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[self.tail]
+        return self.queue[self.rear_index]
 
     def isEmpty(self) -> bool:
         return self.current_length == 0
 
     def isFull(self) -> bool:
         return self.current_length == self.max_length
-
-
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
