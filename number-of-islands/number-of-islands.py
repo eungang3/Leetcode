5b41083b0b6eedc857b2e7ea680c7f02bf46434b
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-27 14:42:46 +0900

    Time: 632 ms, Memory: 16.1 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -11,7 +11,7 @@ class Solution:
             directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
             
             for direction in directions:
-                new_i, new_j = (row + direction[0], column + direction[1])
+                new_i, new_j = row + direction[0], column + direction[1]
                 if (0 <= new_i < row_length) and (0 <= new_j < column_length) and grid[new_i][new_j] == '1':
                     grid[new_i][new_j] = 0
                     queue.append((new_i, new_j))
