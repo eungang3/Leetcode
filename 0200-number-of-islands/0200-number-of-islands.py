1bbda71e3ff2832e34d5f652d84f417f63f91350
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-27 14:04:29 +0900

    Time: 919 ms (21.75%), Space: 16.3 MB (91.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,21 +2,27 @@ from collections import deque
 
 class Solution:
     def bfs(self, grid, i, j):
+        # 큐에 현재 i, j 튜플 추가
         queue = deque([(i, j)])
         n, m = len(grid), len(grid[0])
         while queue:
             row, col = queue.popleft()
-            dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-            for move in dirs:
-                row_move, col_move = (row + move[0], col + move[1])
-                if (0 <= row_move < n) and (0 <= col_move < m) and grid[row_move][col_move] == '1':
-                    grid[row_move][col_move] = 0
-                    queue.append((row_move, col_move))
+            # 우측, 아랫쪽, 좌측, 윗쪽 방향 
+            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            for direction in directions:
+                new_i, new_j = (row + direction[0], col + direction[1])
+                # 한 칸 이동했는데 거기도 1이면
+                if (0 <= new_i < n) and (0 <= new_j < m) and grid[new_i][new_j] == '1':
+                    # 이동한 곳을 0으로 바꿈
+                    grid[new_i][new_j] = 0
+                    # 큐에 이동한 지점 추가
+                    queue.append((new_i, new_j))
                     
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
+                # 현재 요소가 1이면 서치 시작
                 if grid[i][j] == '1':
                     count += 1
                     self.bfs(grid, i, j)
