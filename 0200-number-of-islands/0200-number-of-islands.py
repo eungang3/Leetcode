033cf03913724a990d0aba4343f6a528d7b3901e
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-27 14:11:18 +0900

    Time: 972 ms (16.44%), Space: 16.3 MB (91.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,7 +4,7 @@ class Solution:
     def bfs(self, grid, i, j):
         # 큐에 현재 i, j 튜플 추가
         queue = deque([(i, j)])
-        n, m = len(grid), len(grid[0])
+        row_length, column_length = len(grid), len(grid[0])
         while queue:
             row, col = queue.popleft()
             # 우측, 아랫쪽, 좌측, 윗쪽 방향 
@@ -12,7 +12,7 @@ class Solution:
             for direction in directions:
                 new_i, new_j = (row + direction[0], col + direction[1])
                 # 한 칸 이동했는데 거기도 1이면
-                if (0 <= new_i < n) and (0 <= new_j < m) and grid[new_i][new_j] == '1':
+                if (0 <= new_i < row_length) and (0 <= new_j < column_length) and grid[new_i][new_j] == '1':
                     # 이동한 곳을 0으로 바꿈
                     grid[new_i][new_j] = 0
                     # 큐에 이동한 지점 추가
