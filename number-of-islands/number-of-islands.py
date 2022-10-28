889375784d4c840f90c73d720df0dde6d11ee218
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-28 13:51:40 +0900

    Time: 334 ms, Memory: 16.2 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,27 +1,28 @@
 from collections import deque 
 
 class Solution:
-    def bfs(self, grid, i, j):
+    def bfs(self, i, j, grid):
         queue = deque([(i, j)])
-        row_length = len(grid)
-        column_length = len(grid[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         
         while queue:
             row, column = queue.popleft()
-            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-            
+        
             for direction in directions:
-                new_i, new_j = row + direction[0], column + direction[1]
-                if (0 <= new_i < row_length) and (0 <= new_j < column_length) and grid[new_i][new_j] == '1':
+                new_i = row + direction[0]
+                new_j = column + direction[1]
+                
+                if 0 <= new_i < len(grid) and 0 <= new_j < len(grid[0]) and grid[new_i][new_j] == '1':
                     grid[new_i][new_j] = 0
                     queue.append((new_i, new_j))
-        
-        
+    
+    
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1':
                     count += 1
-                    self.bfs(grid, i, j)
+                    self.bfs(i, j, grid)
+        
         return count
