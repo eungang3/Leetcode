22f144031b35389870d63fd1bfdc8b8c13130d06
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-10-29 17:55:29 +0900

    Time: 1173 ms (60.64%), Space: 198.6 MB (5.02%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        if n < 2:
+            return n
+        
+        queue = collections.deque([n])
+        depth = 0
+        
+        while queue:
+            depth += 1
+            
+            for _ in range(len(queue)):
+                current = queue.popleft()
+                base = 1
+                
+                while base * base <= current:
+                    rest = current - base * base
+                    if rest == 0:
+                        return depth
+                    queue.append(rest)
+                    base += 1
+                
+        return count
