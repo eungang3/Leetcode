836605105a431cd4cd80ff6a2735679859766f48
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-02 16:48:58 +0900

    Time: 143 ms (53.10%), Space: 14.6 MB (20.03%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,47 +1,49 @@
 class MyCircularQueue:
     def __init__(self, k: int):
-        self.max_length = k
-        self.current_length = 0
-        self.queue = [None] * k 
-        self.front_index = -1 
-        self.rear_index = -1 
-
+        self.front = -1
+        self.rear = -1
+        self.max_size = k
+        self.length = 0
+        self.queue = [None] * k
+        
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
         
-        self.rear_index = (self.rear_index + 1) % self.max_length
-        self.queue[self.rear_index] = value
-        self.current_length += 1
-        if self.current_length == 1:
-            self.front_index = 0
-
+        self.rear = (self.rear + 1) % self.max_size
+        self.queue[self.rear] = value
+        self.length += 1
+        
+        if self.length == 1:
+            self.front = 0
+        
         return True
-
+        
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
         
-        self.front_index = (self.front_index + 1) % self.max_length
-        self.current_length -= 1
+        self.front = (self.front + 1) % self.max_size
+        self.length -= 1
+        
         if self.isEmpty():
-            self.front_index = -1
-            self.rear_index = -1
+            self.front = -1
+            self.rear = -1
         
         return True
-
+        
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[self.front_index]
-
+        return self.queue[self.front]
+    
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[self.rear_index]
+        return self.queue[self.rear]
 
     def isEmpty(self) -> bool:
-        return self.current_length == 0
-
+        return self.length == 0
+        
     def isFull(self) -> bool:
-        return self.current_length == self.max_length
+        return self.length == self.max_size
