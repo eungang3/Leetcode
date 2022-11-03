4aba31d1becaa1172f4b77defed731ad9bf13e8b
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-03 17:08:51 +0900

    Time: 46 ms, Memory: 13.9 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.py
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -9,12 +9,13 @@ class Solution:
                 i += 1
                 
             else:
-                current = [1]
                 prev = result[i - 1]
+                current = [1]
                 for j in range(len(prev)):
                     if j + 1 < len(prev):
                         current.append(prev[j] + prev[j + 1])
                 current.append(1)
                 result.append(current)
                 i += 1
+                
         return result[rowIndex]
