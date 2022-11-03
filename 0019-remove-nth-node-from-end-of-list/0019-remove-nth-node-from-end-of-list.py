84e0790be39cfff5be4eb0ed1315152cb7de09b9
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-03 15:54:03 +0900

    Time: 77 ms (8.45%), Space: 14 MB (21.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -9,18 +9,15 @@ class Solution:
         fast = head
         
         for i in range(n):
-            # fast의 다음칸이 없는데 i가 n-1과 같다는 것은
-            # 노드가 두 개 있는데 첫번째 노드를 지우라는 뜻
-            # head가 next 가리키게 한 다음 head 반환
             if not fast.next:
-                if i == n - 1:
+                if n - 1 == i:
                     head = head.next
-                return head
+                    return head
             fast = fast.next
-        
+            
         while fast.next:
-            fast = fast.next
             slow = slow.next
+            fast = fast.next
             
         slow.next = slow.next.next
         return head
