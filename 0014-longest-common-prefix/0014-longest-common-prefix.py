060e30271157253d666f11bd6b6d12dfa6b77d9f
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-04 02:02:01 +0900

    Time: 35 ms (94.80%), Space: 14 MB (49.79%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,12 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs.sort()
-        prefix = ''
+        prefix = strs[0]
         
-        for i in range(len(strs[0])):
-            if strs[0][i] == strs[-1][i]:
-                prefix += strs[0][i]
-            else:
-                break
-        
+        for str in strs:
+            while not str.startswith(prefix):
+                prefix = prefix[:-1]
+                
         return prefix
