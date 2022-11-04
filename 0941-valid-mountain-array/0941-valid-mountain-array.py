94f2bf018feb0dd44363c3e7b4ac78aac9e9d824
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-05 00:32:54 +0900

    Time: 534 ms (29.66%), Space: 15.2 MB (96.23%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -6,10 +6,10 @@ class Solution:
         left = 0
         right = len(arr) - 1
         
-        while left + 1 < len(arr) - 1 and arr[left] < arr[left + 1]:
+        while left < len(arr) - 2 and arr[left] < arr[left + 1]:
             left += 1
         
-        while 0 < right - 1 and arr[right] < arr[right - 1]:
+        while 1 < right and arr[right] < arr[right - 1]:
             right -= 1
             
         return left == right
