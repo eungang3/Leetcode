0abc0e54571b01ef00fb63a0f45d257b69892f64
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-08 11:56:52 +0900

    Time: 896 ms (34.02%), Space: 16.2 MB (97.80%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,22 @@
-from collections import deque
-
 class Solution:
-    def bfs(self, grid, i, j):
-        queue = deque([(i, j)])
-        row_length, column_length = len(grid), len(grid[0])
+    def findLand(self, grid, i, j):
+        stack = [(i, j)]
+        visited = set()
         
-        while queue:
-            row, col = queue.popleft()
+        while stack:
+            i,j = stack.pop()
+        
             directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
             for direction in directions:
-                new_i, new_j = row + direction[0], col + direction[1]
-                
-                if 0 <= new_i < row_length and 0 <= new_j < column_length and grid[new_i][new_j] == '1':
-                    grid[new_i][new_j] = 0
-                    queue.append((new_i, new_j))
+                new_i = direction[0] + i
+                new_j = direction[1] + j
+
+                if 0 <= new_i < len(grid) and 0 <= new_j < len(grid[0]) and grid[new_i][new_j] == '1':
+                    grid[new_i][new_j] = '0'
+                    stack.append((new_i, new_j))
+                    visited.add((new_i, new_j))
+            
     
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
@@ -21,5 +24,6 @@ class Solution:
             for j in range(len(grid[i])):
                 if grid[i][j] == '1':
                     count += 1
-                    self.bfs(grid, i, j)
-        return count
+                    self.findLand(grid, i, j)
+                    
+        return count 
