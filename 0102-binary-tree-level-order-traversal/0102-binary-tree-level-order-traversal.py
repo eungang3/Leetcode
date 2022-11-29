49339537967f756ba8374fc4886b617ce14bfc8e
Yeseul Cho <47664802+eungang3@users.noreply.github.com>
2022-11-29 18:09:31 +0900

    Time: 80 ms (11.45%), Space: 14.2 MB (52.07%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -5,55 +5,30 @@
 #         self.left = left
 #         self.right = right
 
-# from collections import deque
-
-# class Solution:
-#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         if not root:
-#             return []
-        
-#         result = []
-#         queue = deque([root])
-        
-#         while queue:
-#             levelLength = len(queue)
-#             levelNodes = []
-            
-#             for i in range(levelLength):
-#                 currentNode = queue.popleft()
-#                 levelNodes.append(currentNode.val)
-                
-#                 if currentNode.left:
-#                     queue.append(currentNode.left)
-                    
-#                 if currentNode.right:
-#                     queue.append(currentNode.right)
-                    
-#             result.append(levelNodes)
-            
-#             return result
-            
 from collections import deque
 
 class Solution:
-    def levelOrder(self, root):
-        # Corner case.
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         
-        nodes = []  # Result nodes.
-        nodeDeque = deque([root])  
-        # BFS on tree using nodeDeque.
-        while nodeDeque:
-            levelLen = len(nodeDeque)
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            levelLength = len(queue)
             levelNodes = []
-            for i in range(levelLen):
-                curNode = nodeDeque.popleft()
-                levelNodes.append(curNode.val)
-                if curNode.left:
-                    nodeDeque.append(curNode.left)
-                if curNode.right:
-                    nodeDeque.append(curNode.right)  
-            nodes.append(levelNodes)
             
-        return nodes
+            for i in range(levelLength):
+                currentNode = queue.popleft()
+                levelNodes.append(currentNode.val)
+                
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                    
+                if currentNode.right:
+                    queue.append(currentNode.right)
+                    
+            result.append(levelNodes)
+            
+        return result
